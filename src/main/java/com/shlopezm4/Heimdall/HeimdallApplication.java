be42b0f35340b749ba50ff68ccf47eb9aceb2e8d
Humberto Lopez Menendez <shlopezm4@gmail.com>
2018-12-13 18:07:02 -0600

    Almost clean, just added a folder

---
+++ src/main/java/com/shlopezm4/Heimdall/HeimdallApplication.java
@@ -0,0 +1,14 @@
+package com.shlopezm4.Heimdall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeimdallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeimdallApplication.class, args);
+	}
+
+}
+
