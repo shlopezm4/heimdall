1c297c6a9f0efc86f18735baa815239bd6c46362
Humberto Lopez Menendez <shlopezm4@gmail.com>
2018-12-15 23:25:52 -0600

    All the files in payload folder

---
+++ src/main/java/com/shlopezm4/Heimdall/payload/LoginRequest.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.shlopezm4.Heimdall.payload;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ *
+ * @author BetioLópez4
+ */
+public class LoginRequest {
+    @NotBlank
+    private String usernameOrEmail;
+
+    @NotBlank
+    private String password;
+
+    public String getUsernameOrEmail() {
+        return usernameOrEmail;
+    }
+
+    public void setUsernameOrEmail(String usernameOrEmail) {
+        this.usernameOrEmail = usernameOrEmail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
