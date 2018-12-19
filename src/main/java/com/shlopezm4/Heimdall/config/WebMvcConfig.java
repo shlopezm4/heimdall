de63e4b1288ed162feb8c3fe143107d7fa5e9787
Humberto Lopez Menendez <shlopezm4@gmail.com>
2018-12-19 17:32:00 -0600

    Finished temporarily

--- src/main/java/com/shlopezm4/Heimdall/config/WebMvcConfig.java
+++ src/main/java/com/shlopezm4/Heimdall/config/WebMvcConfig.java
@@ -5,10 +5,24 @@
  */
 package com.shlopezm4.Heimdall.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 /**
  *
  * @author BetioLópez4
  */
-public class WebMvcConfig {
-    
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final long MAX_AGE_SECS = 3600;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
+                .maxAge(MAX_AGE_SECS);
+    }
 }
